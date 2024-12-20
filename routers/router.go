de26cb2ffc91af38c6c38d272c9c6e2132dc9b61
aa-nadim <nadim.ice.nstu@gmail.com>
2024-12-21 02:49:29 +0600

    Remove all, Start from scratch

--- routers/router.go
+++ routers/router.go
@@ -2,19 +2,9 @@ package routers
 
 import (
 	"cat-connect/controllers"
-
 	beego "github.com/beego/beego/v2/server/web"
 )
 
 func init() {
-	beego.Router("/", &controllers.BreedsController{})
-
-	// API routes
-	beego.Router("/api/breeds", &controllers.BreedsController{}, "get:GetBreeds")
-	beego.Router("/api/breeds/:id", &controllers.BreedsController{}, "get:GetBreedImages")
-
-	// Favorites routes
-	beego.Router("/api/favorites", &controllers.FavsController{}, "get:GetFavorites")
-	beego.Router("/api/favorites/:favorite_id", &controllers.FavsController{}, "delete:DeleteFavorite")
-	beego.Router("/api/favorites", &controllers.VotingController{}, "post:AddFavorite")
+    beego.Router("/", &controllers.MainController{})
 }
